1b510fbbffecda22012785ce7b1b1e80efd505cb
Jhorman Caballero <jo.caballero@uniandes.edu.co>
2021-04-16 22:51:47 -0500

    Final changes

--- scripts/dataCourses.js
+++ scripts/dataCourses.js
@@ -1,8 +1,13 @@
 import { Course } from './course.js';
 export var dataCourses = [
-    new Course("Ingeniería de Sw", "Pablo Picasso", 4),
-    new Course("Futbol 1", "Freddy Rincón", 2),
-    new Course("Algoritmos", "Carlos Fuentes", 2),
-    new Course("Estructuras de Datos", "Yesid D", 1),
-    new Course("Futbol 2", "James R", 6)
+    new Course("ANADEC", "Miguel Garcia", 3),
+    new Course("CBU Biodiversidad", "Juan Sanchez", 2),
+    new Course("Calculo Vectorial", "Mauricio Velasco", 3),
+    new Course("Desarrollo de SW en Equipo", "Cesar Forero", 3),
+    new Course("DALGO", "Rodrigo Cardozo", 3),
+    new Course("English 07", "Carlos Arias", 0),
+    new Course("English 08", "Adriana Castañeda", 0),
+    new Course("Probabilidad I", "Daniel Suarez", 3),
+    new Course("Sistemas Transaccionales", "German Bravo", 3),
+    new Course("Trabajo Asistido Probabilidad", "Natalia Pacheco", 0)
 ];
---
+++ students.ts
@@ -0,0 +1,5 @@
+import { Student } from './student.js';
+
+export const students = [
+    new Student("images/Memoji.jpeg", 201812920, 1004922494, 19, 'Calle 19 #2A-10', 5849585)
+]
